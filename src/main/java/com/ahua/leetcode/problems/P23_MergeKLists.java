47960e18ab450c3885c9c2f2c72a3f511e273971
chnahua <chnahua@foxmail.com>
2021-10-18 15:52:38 +0800

    修改 P23_MergeKLists

--- src/main/java/com/ahua/leetcode/problems/P23_MergeKLists.java
+++ src/main/java/com/ahua/leetcode/problems/P23_MergeKLists.java
@@ -3,6 +3,8 @@ package com.ahua.leetcode.problems;
 /**
  * @author huajun
  * @create 2021-10-18 14:16
+ * <p>
+ * 合并 K 个升序链表,与 JZOII078 是同一个题
  */
 
 /**
@@ -48,34 +50,34 @@ public class P23_MergeKLists {
 }
 
 class P23_Solution {
-    // 有奇数个链表,先将最后一个链表合并到第一个链表中,就可以看作有偶数个链表
-    // 将偶数(2t)个链表分别两两合并(第 i 个和第i +  t个)成为前(t)个链表, 类似于归并排序
+    // 分治合并
     public ListNode mergeKLists(ListNode[] lists) {
-        int t = lists.length;
+        int k = lists.length;
         // 链表为空，直接返回
-        if (t == 0) {
+        if (k == 0) {
             return null;
         }
 
         while (true) {
-            if (t == 1) {
+            if (k == 1) {
                 return lists[0];
-            } else if (t == 2) {
-                return mergeTwoLists(lists[0], lists[t - 1]);
+            } else if (k == 2) {
+                return mergeTwoLists(lists[0], lists[k - 1]);
             }
             // 有奇数个链表,先将最后一个链表合并到第一个链表中,就可以看作有偶数个链表
-            if (t % 2 == 1) {
-                lists[0] = mergeTwoLists(lists[0], lists[t - 1]);
+            if (k % 2 == 1) {
+                lists[0] = mergeTwoLists(lists[0], lists[k - 1]);
             }
-            t = t / 2;
-            // 将偶数(2t)个链表分别两两合并(第 i 个和第i +  t个)成为前(t)个链表, 类似于归并排序
-            for (int i = 0; i < t; i++) {
-                lists[i] = mergeTwoLists(lists[i], lists[i + t]);
+            k = k / 2;
+            // 将偶数(2k)个链表分别两两合并(第 i 个和第 i + k 个)成为前(k)个链表, 类似于归并排序——分治
+            for (int i = 0; i < k; i++) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + k]);
             }
         }
     }
-    // 将其它链表依次插入到第一个链表中，类似于插入排序
-    public ListNode mergeKLists1(ListNode[] lists) {
+
+    // 顺序合并
+    public ListNode mergeKLists2(ListNode[] lists) {
         int len = lists.length;
         if (len == 0) {
             // 链表为空,直接返回
@@ -85,7 +87,7 @@ class P23_Solution {
             return lists[0];
         }
 
-        // 将其它链表依次插入到第一个链表中，类似于插入排序
+        // 将其它链表依次插入到第一个链表中，类似于插入排序,性能不如前一个
         for (int i = 1; i < len; i++) {
             lists[0] = mergeTwoLists(lists[0], lists[i]);
         }
@@ -93,6 +95,7 @@ class P23_Solution {
         return lists[0];
     }
 
+    // 我的 mergeTwoLists
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) {
             list1 = list2;
@@ -139,10 +142,121 @@ class P23_Solution {
         }
         return toList;
     }
+
+    // 官方的 mergeTwoLists, 实现更简洁
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        if (list1 == null || list2 == null) {
+            return list1 != null ? list1 : list2;
+        }
+
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = list1 == null ? list2 : list1;
+        return head.next;
+    }
 }
 
-class P23_Solution1 {
+class P23_Solution_1 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int len = lists.length;
+        if (len == 0) {
+            return null;
+        } else if (len == 1) {
+            return lists[0];
+        }/* else if (len == 2) {
+            return mergeTwoLists(lists[0], lists[len - 1]);
+        }*/
+//        ListNode temp = new ListNode();
+//        ListNode mergedList = new ListNode();
+//        ListNode mergedListTailNode = new ListNode();
+//        mergedList.next = mergedListTailNode;
+//        if (len % 2 == 1) {
+//            lists[0] = mergeTwoLists(lists[0], lists[len - 1]);
+//        }
 
+        int t = len;
+
+        while (t != 0) {
+            t = t / 2;
+            for (int i = 0; i < t; i++) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + t]);
+            }
+            if (t % 2 == 1) {
+                lists[0] = mergeTwoLists(lists[0], lists[t - 1]);
+            }
+        }
+
+//        for (int i = 1; i < len; i++) {
+//            lists[0] = mergeTwoLists(lists[0], lists[i]);
+//        }
+
+//        for (int i = 0; i < len; i++) {
+////            if (lists[i].val <= lists[i + 1].val) {
+////                temp = lists[i];
+////                lists[i] = lists[i].next;
+////                temp.next = null;
+////                mergedListTailNode.next = temp;
+////                mergedListTailNode = temp;
+////            }
+//        }
+        return lists[0];
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            list1 = list2;
+            return list1;
+        } else if (list2 == null) {
+            return list1;
+        }
+        ListNode fromList;
+        ListNode toList;
+
+        if (list1.val <= list2.val) {
+            // pre.next = list1;
+            fromList = list2;
+            toList = list1;
+        } else {
+//            pre = list2;
+//            list2 = list2.next;
+//            pre.next = list1;
+//            list1 = pre;
+//            pre.next = list1;
+            fromList = list1;
+            toList = list2;
+        }
+        ListNode pre = new ListNode();
+        pre.next = toList;
+        ListNode cur = toList;
+        ListNode temp;
+        while (fromList != null && cur != null) {
+            if (cur.val <= fromList.val) {
+                pre = cur;
+                cur = cur.next;
+            } else {
+                temp = fromList.next;
+                pre.next = fromList;
+                fromList.next = cur;
+                pre = fromList;
+                fromList = temp;
+            }
+        }
+        if (cur == null) {
+            pre.next = fromList;
+        }
+        return toList;
+    }
 }
 
 // Definition for singly-linked list.
