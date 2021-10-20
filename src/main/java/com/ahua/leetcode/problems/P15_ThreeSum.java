40d56ae1eed73dbb821df5c80e714d0c68f79d39
chnahua <chnahua@foxmail.com>
2021-10-20 22:40:16 +0800

    P15_未完成

---
+++ src/main/java/com/ahua/leetcode/problems/P15_ThreeSum.java
@@ -0,0 +1,28 @@
+package com.ahua.leetcode.problems;
+
+/**
+ * @author huajun
+ * @create 2021-10-19 16:35
+ */
+
+import java.util.List;
+
+/**
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
+ * 请你找出所有和为 0 且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ */
+public class P15_ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,0,1,2,-1,-4}; // 输出: [[-1,-1,2],[-1,0,1]]
+//        P15_Solution solution = new P15_Solution();
+//        System.out.println(solution.threeSum(nums));
+    }
+}
+
+//class P15_Solution {
+//    public List<List<Integer>> threeSum(int[] nums) {
+//        // List<Integer> list = new List<>();
+//    }
+//}
