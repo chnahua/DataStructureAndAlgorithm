51c4a9a4b969987ef626afbb5acbd472eb7ae040
chnahua <chnahua@foxmail.com>
2021-11-05 16:14:18 +0800

    P42_Trap

--- src/main/java/com/ahua/leetcode/problems/P42_Trap.java
+++ src/main/java/com/ahua/leetcode/problems/P42_Trap.java
@@ -5,6 +5,13 @@ package com.ahua.leetcode.problems;
  * @create 2021-11-03 17:37
  */
 
+/**
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ * n == height.length
+ * 1 <= n <= 2 * 104
+ * 0 <= height[i] <= 105
+ */
 public class P42_Trap {
     public static void main(String[] args) {
         int[] height = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
