05749e30272358ed9b088024ca13e30886a71cd0
chnahua <chnahua@foxmail.com>
2021-11-16 18:56:20 +0800

    P300_LengthOfLIS

--- src/main/java/com/ahua/leetcode/problems/P300_LengthOfLIS.java
+++ src/main/java/com/ahua/leetcode/problems/P300_LengthOfLIS.java
@@ -35,23 +35,42 @@ class P300_Solution {
     // 动态规划
     // 时间复杂度：O(n^2) 空间复杂度：O(n)
     // 内层 for 循环我犯过一次错误, 以为在 nums[i] 前找到第一个比 nums[i] 小的值的 dp 就行了
-    public int lengthOfLIS1(int[] nums) {
+    public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         if (n == 0) {
             return 0;
         } else if (n == 1) {
             return 1;
         }
+        // 最长递增子序列的长度, 初值为 0 为 1 均可
+        int maxLen = 1;
+        // 以当前第 i 个数 nums[i] 结尾的最长子序列的长度
         int[] dp = new int[n];
+        // 赋初值: 以 nums[0] 结尾的最长子序列的长度为 1
         dp[0] = 1;
-        int maxLen = 0;
+        // 从 i == 1 开始
         for (int i = 1; i < n; i++) {
+            // 在 [0,i) 之间找到比当前 nums[i] 小的那个数, 假设有 x 个这样的数
+            // 那么 dp[i] 也就等于以这 x 个这样的数结尾的最长子序列长度的最大值 + 1
+            // 也就是找到一条 i 之前的最长子序列, 并且该最长子序列的结尾值要 nums[j] < nums[i]
+            // 那么现在加上 nums[i] 这个数, 就有了一条以 nums[i] 结尾的最长子序列
+            // 同时也知道了其长度值, 更新 dp[i]
+            // 那么当循环结束时, 就知道了以这 n 个数结尾的最长子序列的长度, 最长长度就很轻松就能得到
+
+            // 以上讨论的是找得到的情况, 如果找不到呢?
+            // 找不到不就说明当前 nums[i] 是 [0,i] 之间这 i + 1 个数中的最大值吗?
+            // 那么 nums[i] 就成为了一条新的子序列的开头(也即末尾), 当前序列长度就为 1 了
+            // 故在查找前, 先令其为 1, 后续如果找到, 肯定值比 1 大
             dp[i] = 1;
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
+                    // 如果是非严格单调递增, 改为 <= 即可
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
+
+            // 每次得到以 nums[i] 结尾的最长子序列长度, 就与之前的 maxLen 比较
+            // 得到 [0,i] 之间以这 i + 1 个数结尾的最长子序列长度中的最大值
             maxLen = Math.max(maxLen, dp[i]);
         }
         // System.out.println(Arrays.toString(dp));
@@ -60,7 +79,7 @@ class P300_Solution {
 
     // 贪心 + 二分查找
     // 时间复杂度：O(nlogn) 空间复杂度：O(n)
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS1(int[] nums) {
         int n = nums.length;
         if (n == 0) {
             return 0;
