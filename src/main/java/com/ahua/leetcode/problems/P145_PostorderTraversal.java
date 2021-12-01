a2aa08c80406e2f883b3283f1ae49815f275d91e
chnahua <chnahua@foxmail.com>
2021-12-01 23:41:27 +0800

    P145_PostorderTraversal

--- src/main/java/com/ahua/leetcode/problems/P145_PostorderTraversal.java
+++ src/main/java/com/ahua/leetcode/problems/P145_PostorderTraversal.java
@@ -1,7 +1,6 @@
 package com.ahua.leetcode.problems;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author huajun
@@ -37,7 +36,7 @@ public class P145_PostorderTraversal {
  */
 
 // 递归实现
-class P145_Solution {
+class P145_Solution1 {
     List<Integer> ans = new ArrayList<>();
 
     public List<Integer> postorderTraversal(TreeNode root) {
@@ -60,5 +59,35 @@ class P145_Solution {
         postorder(node.right);
         // 添加该结点入链表
         ans.add(node.val);
+    }
+}
+
+// 迭代
+class P145_Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> ans = new LinkedList<>();
+        // 特殊情况提前处理
+        if (root == null) {
+            return ans;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+
+        while (root != null || !stack.isEmpty()) {
+            // 一直遍历右结点
+            if (root != null) {
+                // 每遇到一个结点, 就把它加入结果集, 并把该节点保存到中间结果中
+                // 每次前序遍历时, 都将结点写入结果链表头, 而不是尾
+                ans.addFirst(root.val);
+                stack.push(root);
+                // 每次先遍历右结点, 走到空, 再遍历左结点
+                root = root.right;
+            } else {
+                // 右子树走到空, 就从获取已经遍历过右子树的中间结果, 将它出栈, 并遍历它的左子树
+                root = stack.poll();
+                root = root.left;
+            }
+        }
+        return ans;
     }
 }
