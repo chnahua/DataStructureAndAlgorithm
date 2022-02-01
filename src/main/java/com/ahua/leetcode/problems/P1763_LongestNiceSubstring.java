7825a5a6e779e276aeadcece9bc17e20566d60c8
chnahua <chnahua@foxmail.com>
2022-02-02 00:40:53 +0800

    P1763_LongestNiceSubstring

--- src/main/java/com/ahua/leetcode/problems/P1763_LongestNiceSubstring.java
+++ src/main/java/com/ahua/leetcode/problems/P1763_LongestNiceSubstring.java
@@ -29,6 +29,7 @@ public class P1763_LongestNiceSubstring {
 }
 
 // 枚举 O(N^2) O(1)
+// 4 ms
 class P1763_Solution1 {
     public String longestNiceSubstring(String s) {
         int n = s.length();
@@ -56,30 +57,68 @@ class P1763_Solution1 {
     }
 }
 
-// 分治 O(N^2) O(1)
+// 分治
+// 0 ms
 class P1763_Solution {
+    private int maxPos;
+    private int maxLen;
+
     public String longestNiceSubstring(String s) {
-        int n = s.length();
-        int maxPos = 0;
-        int maxLen = 0;
-        for (int i = 0; i < n; i++) {
-            // lower 和 upper 的每一位表示是否有小写字母和大写字母
-            int lower = 0;
-            int upper = 0;
-            for (int j = i; j < n; j++) {
-                char ch = s.charAt(j);
-                if (Character.isLowerCase(ch)) {
-                    lower |= 1 << (ch - 'a');
-                } else {
-                    upper |= 1 << (ch - 'A');
-                }
-                // 相等时表示同一字母的小写字母和大写字母同时存在
-                if (lower == upper && j - i + 1 > maxLen) {
-                    maxPos = i;
-                    maxLen = j - i + 1;
-                }
-            }
-        }
+        this.maxPos = 0;
+        this.maxLen = 0;
+        dfs(s, 0, s.length() - 1);
         return s.substring(maxPos, maxPos + maxLen);
     }
+
+    private void dfs(String s, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        // lower 和 upper 的每一位表示是否有小写字母和大写字母
+        int lower = 0;
+        int upper = 0;
+        for (int i = start; i <= end; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                lower |= 1 << (ch - 'a');
+            } else {
+                upper |= 1 << (ch - 'A');
+            }
+        }
+        // 相等时表示同一字母的小写字母和大写字母同时存在
+        if (lower == upper) {
+            if (end - start + 1 > maxLen) {
+                maxPos = start;
+                maxLen = end - start + 1;
+            }
+            return;
+        }
+        // valid 表示大小写字母均存在的字母集合
+        int valid = lower & upper;
+        int pos = start;
+        while (pos <= end) {
+            start = pos;
+            // pos 位置处的这个字母, 它的大小写字母在该子串中均存在
+            while (pos <= end && (valid & (1 << Character.toLowerCase(s.charAt(pos)) - 'a')) != 0) {
+                pos++;
+            }
+            // 退出 while 时 pos 处的这个字母大小写只出现其中之一
+            // 遍历这个小子串, 是否为美好子串
+            dfs(s, start, pos - 1);
+            pos++;
+        }
+//        // 使用 for 循环
+//        for (int pos = start; pos <= end; pos++) {
+//            start = pos;
+//            // pos 位置处的这个字母, 它的大小写字母在该子串中均存在
+//            while (pos <= end && (valid & (1 << Character.toLowerCase(s.charAt(pos)) - 'a')) != 0) {
+//                pos++;
+//            }
+//            // 退出 while 时 pos 处的这个字母大小写只出现其中之一
+//            // 遍历这个小子串, 是否为美好子串
+//            dfs(s, start, pos - 1);
+//        }
+    }
 }
+
+// 滑动窗口
