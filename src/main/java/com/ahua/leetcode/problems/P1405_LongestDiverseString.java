6786eccd4b2e03266250bf8176207a94e93eb91b
chnahua <chnahua@foxmail.com>
2022-02-08 02:32:27 +0800

    P1405_LongestDiverseString

--- src/main/java/com/ahua/leetcode/problems/P1405_LongestDiverseString.java
+++ src/main/java/com/ahua/leetcode/problems/P1405_LongestDiverseString.java
@@ -29,23 +29,29 @@ public class P1405_LongestDiverseString {
     }
 }
 
+// 不知道采取了何种方法, 看了官方题解后, 原来是贪心
+// 0ms 100%
 class P1405_Solution {
-
+    int[] abcNum;
+    // 对应于 abcNum 中保存的 a,b,c 的下标
     static final int A = 0;
     static final int B = 1;
     static final int C = 2;
-    int[] abcNum;
 
     public String longestDiverseString(int a, int b, int c) {
+        // 可删除
         if (a == 0 && b == 0 && c == 0) {
             return "";
         }
         this.abcNum = new int[]{a, b, c};
         final String[] str = new String[]{"a", "b", "c", "aa", "bb", "cc"};
-        StringBuilder sb = new StringBuilder("");
+        StringBuilder sb = new StringBuilder();
         int addStrIndex = getStrIndex(-1);
         while (addStrIndex != -1) {
+            // 添加所剩字符最多的一个或者两个字符到末尾
             sb.append(str[addStrIndex]);
+            // 得到下一次是添加到末尾的字符是哪一种, 是一个还是两个
+            // pre 表示上一次添加的是 abc 中的哪种字符(下标表示), 函数返回下一次要添加的字符以及个数(对应于 str 数组的索引)
             addStrIndex = getStrIndex(addStrIndex % 3);
         }
         return sb.toString();
@@ -142,72 +148,50 @@ class P1405_Solution {
         int a = abcNum[A];
         int b = abcNum[B];
         int c = abcNum[C];
-        int max = -1;
-        int mid = -1;
-        if (a == b && a == c && a == 0) {
+        // 最多字符和次多字符的位置(0,1,2)
+        int max, mid;
+        if (a == 0 && b == 0 && c == 0) {
             return -1;
         }
         // a b c 的大小关系
         if (a >= b && a >= c) {
             // a 最大且 a 不等于 0
+            // a >= b >= c 或 a >= c >= b
             max = A;
-            if (max != pre) {
-                if (a >= 2) {
-                    abcNum[max] -= 2;
-                    return max + 3;
-                } else if (a == 1) {
-                    abcNum[max]--;
-                    return max;
-                }
-            }
-            if (b >= c) {
-                // a >= b >= c
-                mid = B;
-            } else {
-                // a >= c >= b
-                mid = C;
-            }
+            mid = b >= c ? B : C;
         } else if (b >= a && b >= c) {
             // b 最大且 b 不等于 0
+            // b >= a >= c 或 b >= c >= a
             max = B;
-            if (max != pre) {
-                if (b >= 2) {
-                    abcNum[max] -= 2;
-                    return max + 3;
-                } else if (b == 1) {
-                    abcNum[max]--;
-                    return max;
-                }
-            }
-            if (a >= c) {
-                // b >= a >= c
-                mid = A;
-            } else {
-                // b >= c >= a
-                mid = C;
-            }
-        } else if (c >= a && c >= b) {
+            mid = a >= c ? A : C;
+        } else {
             // c 最大且 c 不等于 0
+            // c >= a >= b 或 c >= b >= a
             max = C;
-            if (max != pre) {
-                if (c >= 2) {
-                    abcNum[max] -= 2;
-                    return max + 3;
-                } else if (c == 1) {
-                    abcNum[max]--;
-                    return max;
-                }
-            }
-            if (a >= b) {
-                // c >= a >= b
-                mid = A;
-            } else {
-                // c >= b >= a
-                mid = B;
+            mid = a >= b ? A : B;
+        }
+        // 返回此次应当添加那种字符以及几个(对应于 str 数组的索引)
+        return func(max, mid, pre);
+    }
+
+    private int func(int max, int mid, int pre) {
+        // max != pre 时, 表示当前最多字符与上一次添加的字符不是同一个字符, 那么接下来这次就可以添加这个现有字符最多的字符
+        // 而添加一个还是两个该字符则由它的个数 abcNum[max] 决定
+        if (max != pre) {
+            if (abcNum[max] >= 2) {
+                abcNum[max] -= 2;
+                return max + 3;
+            } else if (abcNum[max] == 1) {
+                abcNum[max]--;
+                return max;
             }
         }
-        // max == pre 时
+        // max == pre 时 表示当前最多字符与上一次添加的字符是同一个字符, 那么此次就不能添加这个上次添加过的字符, 而应当添加次多的字符
+        // 而添加一个还是两个该字符则由它的个数 abcNum[mid] 以及与最多字符的个数 abcNum[max] 之间的关系共同决定
         if (abcNum[mid] > 2) {
+            // 这一步是关键, < 或者 <= 都正确
+            // 如果最多字符的个数 abcNum[max] 比次多字符的个数的两倍都多, 那么就要减缓添加次多字符的速度, 即只添加一个
+            // 至于为何这样, 我也没有一个理性的认识, 只是感觉如此
             if (abcNum[mid] * 2 < abcNum[max]) {
                 abcNum[mid]--;
                 return mid;
@@ -216,10 +200,15 @@ class P1405_Solution {
                 return mid + 3;
             }
         } else if (abcNum[mid] == 1 || abcNum[mid] == 2) {
-            // 此处当 abcNum[mid] == 2 时, 不要一次添加两个字符, 否则会出错(0,8,11)、(0,2,4)、(0,2,3)
+            // 此处当 abcNum[mid] == 2 时这种情况, 不要将其放到上个条件判断中
+            // 在上个条件判断中, 在最后 'b' 字符个数为 2 时, 会一次添加两个字符, 这样就出错了
+            // 错误案例(0,8,11)
+            // (0,2,4)、(0,2,3) 这两个案例是最后的某种状态, 之前添加的是 'c', 现在应当添加 'b'
+            // 而如果一次添加两个 'b', 最终 'c' 就会有剩余(未添加)
             abcNum[mid]--;
             return mid;
         } else {
+            // 次多字符个数为 0, 返回 -1, 不能再添加了
             return -1;
         }
     }
